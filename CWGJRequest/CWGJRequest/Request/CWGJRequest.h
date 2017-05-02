114a38393a8ce51fe1916c53ccf1e617dfe47b47
renxinwei <362922604@qq.com>
2017-05-02 18:14:37 +0800

    add request class

---
+++ CWGJRequest/CWGJRequest/Request/CWGJRequest.h
@@ -0,0 +1,13 @@
+//
+//  CWGJRequest.h
+//  CWGJRequest
+//
+//  Created by renxinwei on 2017/5/2.
+//  Copyright © 2017年 cheweiguanjia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CWGJRequest : NSObject
+
+@end
---
+++ CWGJRequest/CWGJRequest/Request/CWGJRequest.m
@@ -0,0 +1,13 @@
+//
+//  CWGJRequest.m
+//  CWGJRequest
+//
+//  Created by renxinwei on 2017/5/2.
+//  Copyright © 2017年 cheweiguanjia. All rights reserved.
+//
+
+#import "CWGJRequest.h"
+
+@implementation CWGJRequest
+
+@end
